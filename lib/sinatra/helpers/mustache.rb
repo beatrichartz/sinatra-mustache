0edace17c2a83bcd35162184f75f6803e953679c
beatrichartz <beat.richartz@gmail.com>
2013-03-27 18:49:13 +0100

    ! refactoring fail

--- lib/sinatra/helpers/mustache.rb
+++ lib/sinatra/helpers/mustache.rb
@@ -5,9 +5,9 @@ module Sinatra
     def mustache_helper *args      
       args.each do |arg|
         if arg.is_a? Symbol
-          add_to_methods(instance_method(arg).bind(self.new!))
+          add_to_methods(arg, instance_method(arg).bind(self.new!))
         elsif arg.is_a? Module
-          arg.instance_methods.each { |m| add_to_methods(m) }
+          arg.instance_methods.each { |m| add_to_methods(arg, m) }
         else
           raise ArgumentError.new("#{arg.class} can not be used with mustache_helper, please provide a symbol or a Module")
         end
@@ -15,8 +15,8 @@ module Sinatra
     end
     alias :mustache_helpers :mustache_helper
     
-    def add_to_methods meth
-      METHODS[arg] = Proxies::Currystache.new(meth)
+    def add_to_methods name, meth
+      METHODS[name] = Proxies::Currystache.new(meth)
     end
     
   end

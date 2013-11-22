f70d1d8edef625776584eeb9066b42683f31f333
beatrichartz <beat.richartz@gmail.com>
2013-11-22 09:45:45 +0000

    . rewrite mustache helper module

--- lib/sinatra/helpers/mustache.rb
+++ lib/sinatra/helpers/mustache.rb
@@ -5,9 +5,9 @@ module Sinatra
     def mustache_helper *args
       args.each do |arg|
         if arg.is_a? Symbol
-          add_to_mustache_helper_methods(arg, instance_method(arg).bind(self.new!))
+          add_to_mustache_helper_methods(arg)
         elsif arg.is_a? Module
-          arg.instance_methods.each { |m| add_to_mustache_helper_methods(m, arg.instance_method(m).bind(self.new!)) }
+          add_to_mustache_helper_methods(*arg.instance_methods, arg)
         else
           raise ArgumentError.new("#{arg.class} can not be used with mustache_helper, please provide a symbol or a Module")
         end
@@ -15,8 +15,17 @@ module Sinatra
     end
     alias :mustache_helpers :mustache_helper
     
-    def add_to_mustache_helper_methods name, meth
-      METHODS[name] = Proxies::Currystache.new(meth)
+    private
+    
+    def add_to_mustache_helper_methods *meths
+      scope = meths.last.is_a?(Module) ? meths.pop : self
+      meths.each do |meth|
+        METHODS[meth] = Proxies::Currystache.new(rebind_method!(meth, scope))
+      end
+    end
+    
+    def rebind_method! meth,scope=self
+      scope.instance_method(meth).bind(self.new!)
     end
     
   end
